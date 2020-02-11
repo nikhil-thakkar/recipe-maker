dae7e31411f186a26b468dd8390de0dd674f9d9c
Nikhil Thakkar <nikhil@backbase.com>
2020-02-11 19:44:44 +0100

    chore: clean the build.gradle for each feature module

--- buildSrc/src/main/kotlin/AndroidModulePlugin.kt
+++ buildSrc/src/main/kotlin/AndroidModulePlugin.kt
@@ -13,6 +13,8 @@ class AndroidModulePlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         if (project.hasProperty("android")) {
+            project.plugins.apply("kotlin-android")
+            project.plugins.apply("kotlin-android-extensions")
             project.configureAndroidBlock()
             project.configureCommonDependencies()
         }
@@ -41,12 +43,17 @@ internal fun Project.configureAndroidBlock() = extensions.getByType<BaseExtensio
 }
 
 internal fun Project.configureCommonDependencies() {
+    //find the app module
+    //replace this with your base module if using otherwise
+    val app = findProject(":app")
     extensions.getByType<BaseExtension>().run {
         dependencies {
             if (!(name == "app" || name == "core")) {
                 // Since the feature modules need to depend on the :app or the base module
                 //Don't add the app to itself or to core
-                add("implementation", project(":app"))
+                if (app != null) {
+                    add("implementation", app)
+                }
             }
             add("testImplementation", Libs.junit)
             add("testImplementation", Libs.mockk)
